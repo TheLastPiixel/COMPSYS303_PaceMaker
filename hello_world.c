d87d551522eacd6ad15873142a03606d08857e5f
TheLastPiixel <48301981+TheLastPiixel@users.noreply.github.com>
2019-09-27 17:13:34 +1200

    Update hello_world.c
    replaced with existing pacemaker c code

--- hello_world.c
+++ hello_world.c
@@ -1,76 +1,769 @@
-/*
- * "Hello World" example.
- *
- * This example prints 'Hello from Nios II' to the STDOUT stream. It runs on
- * the Nios II 'standard', 'full_featured', 'fast', and 'low_cost' example
- * designs. It runs with or without the MicroC/OS-II RTOS and requires a STDOUT
- * device in your system's hardware.
- * The memory footprint of this hosted application is ~69 kbytes by default
- * using the standard reference design.
- *
- * For a reduced footprint version of this template, and an explanation of how
- * to reduce the memory footprint for a given application, see the
- * "small_hello_world" template.
- *
- */
-
-
-#include <alt_types.h> // alt_u32 is a kind of alt_types
-#include <sys/alt_alarm.h>
-#include <sys/alt_irq.h> // to register interrupts
-#include <system.h>
-#include <altera_avalon_pio_regs.h>
-#include <stdio.h>
-#include "pacemaker.h"
-
-#define GLED7 128
-#define GLED6 64
-#define GLED5 32
-#define GLED4 16
-#define GLED3 8
-#define GLED2 4
-#define GLED1 2
-#define GLED0 1
-
-//Timeout values in milliseconds
-#define AVI_VALUE 300
-#define AEI_VALUE 800
-#define PVARP_VALUE 50
-#define VRP_VALUE 150
-#define LRI_VALUE 950
-#define URI_VALUE 900
-
-int main()
-{
-	//Turns on LEDs based on TLC
-	IOWR_ALTERA_AVALON_PIO_DATA(LEDS_GREEN_BASE, 1);
-
-	reset();
-
-	//Variable Declaration
-	int LED = 0;
-
-	to_avi = 300;
-	to_lri = 950;
-	to_uri = 900;
-	to_pvarp = 50;
-	to_vrp = 150;
-	to_aei = 800;
-
-
-	while(1) {
-
-
-		if (ap == 1) {
-			LED = LED + GLED0;
-		}
-
-		if (vp == 1) {
-			LED = LED + GLED1;
-		}
-
-		LED = 0;
-
-		tick();
-	}
+/*****************************************************************************/
+/*                 G E N E R A T E D       C    C O D E                      */
+/*****************************************************************************/
+/* KIELER - Kiel Integrated Environment for Layout Eclipse RichClient        */
+/*                                                                           */
+/* http://www.informatik.uni-kiel.de/rtsys/kieler/                           */
+/* Copyright 2014 by                                                         */
+/* + Kiel University                                                         */
+/*   + Department of Computer Science                                        */
+/*     + Real-Time and Embedded Systems Group                                */
+/*                                                                           */
+/* This code is provided under the terms of the Eclipse Public License (EPL).*/
+/*****************************************************************************/
+char as;
+char vs;
+char ap;
+char vp;
+int to_lri;
+int to_uri;
+int to_avi;
+int to_pvarp;
+int to_vrp;
+int to_aei;
+char LRI;
+char URI;
+char AVI;
+char PVARP;
+char VRP;
+char AEI;
+char LRI_p;
+char URI_p;
+char AVI_p;
+char AEI_p;
+char as_accept;
+char vs_accept;
+char ap_accept;
+char vp_accept;
+int t_lri;
+int t_uri;
+int t_avi;
+int t_pvarp;
+int t_vrp;
+int t_aei;
+char _trig;
+char _trig2;
+char _trig3;
+char _trig4;
+char _trig5;
+char _trig6;
+char g0;
+char g1;
+char g2;
+char g3;
+char PRE_g3;
+char g4;
+char g4b;
+char g5;
+char g6;
+char PRE_g6;
+char g7;
+char g8;
+char g9;
+char g10;
+char PRE_g10;
+char g11;
+char g12;
+char g13;
+char g14;
+char g15;
+char PRE_g15;
+char g16;
+char g16b;
+char g17;
+char g18;
+char PRE_g18;
+char g19;
+char g20;
+char g21;
+char g22;
+char PRE_g22;
+char g23;
+char g24;
+char g25;
+char g26;
+char g27;
+char g28;
+char g29;
+char g30;
+char PRE_g30;
+char g31;
+char g32;
+char g33;
+char PRE_g33;
+char g34;
+char g34b;
+char g35;
+char g36;
+char g37;
+char PRE_g37;
+char g38;
+char g39;
+char g40;
+char PRE_g40;
+char g41;
+char g41b;
+char g42;
+char g43;
+char g44;
+char g45;
+char g46;
+char g47;
+char g48;
+char g49;
+char PRE_g49;
+char g50;
+char g51;
+char g52;
+char PRE_g52;
+char g53;
+char g54;
+char g55;
+char g56;
+char g57;
+char g58;
+char g59;
+char PRE_g59;
+char g60;
+char g60b;
+char g61;
+char PRE_g61;
+char g62;
+char g63;
+char g64;
+char PRE_g64;
+char g65;
+char g66;
+char g67;
+char g68;
+char g69;
+char g70;
+char g71;
+char g72;
+char g73;
+char g74;
+char PRE_g74;
+char g75;
+char g76;
+char g77;
+char PRE_g77;
+char g78;
+char g78b;
+char g79;
+char g80;
+char g81;
+char PRE_g81;
+char g82;
+char g83;
+char g84;
+char PRE_g84;
+char g85;
+char g86;
+char g87;
+char g88;
+char g89;
+char g90;
+char g91;
+char g92;
+char g93;
+char g94;
+char PRE_g94;
+char g95;
+char g96;
+char g97;
+char PRE_g97;
+char g98;
+char g98b;
+char g99;
+char g100;
+char g101;
+char PRE_g101;
+char g102;
+char g103;
+char g104;
+char PRE_g104;
+char g105;
+char g105b;
+char g106;
+char g107;
+char g108;
+char PRE_g108;
+char g109;
+char g109b;
+char g110;
+char g111;
+char g112;
+char g113;
+char g114;
+char g115;
+char PRE_g115;
+char g116;
+char g116b;
+char g117;
+char PRE_g117;
+char g118;
+char g119;
+char g120;
+char PRE_g120;
+char g121;
+char g121b;
+char g122;
+char g123;
+char g124;
+char g125;
+char g126;
+char g127;
+char g128;
+char PRE_g128;
+char g129;
+char g130;
+char g131;
+char PRE_g131;
+char g132;
+char g133;
+char g134;
+char g135;
+char g136;
+char g137;
+char g138;
+char g139;
+char g140;
+char g141;
+char g142;
+char g143;
+char PRE_g143;
+char g144;
+char g145;
+char g146;
+char PRE_g146;
+char g147;
+char g147b;
+char g148;
+char g149;
+char g150;
+char PRE_g150;
+char g151;
+char g152;
+char g153;
+char PRE_g153;
+char g154;
+char g155;
+char g156;
+char g157;
+char g158;
+char _GO;
+char _cg7;
+char _cg11;
+char _cg8;
+char _cg4;
+char _cg19;
+char _cg23;
+char _cg20;
+char _cg16;
+char _cg41;
+char _cg28;
+char _cg34;
+char _cg38;
+char _cg44;
+char _cg42;
+char _cg47;
+char _cg53;
+char _cg65;
+char _cg60;
+char _cg62;
+char _cg66;
+char _cg85;
+char _cg72;
+char _cg78;
+char _cg82;
+char _cg86;
+char _cg105;
+char _cg110;
+char _cg92;
+char _cg98;
+char _cg109;
+char _cg102;
+char _cg111;
+char _cg106;
+char _cg116;
+char _cg118;
+char _cg135;
+char _cg121;
+char _cg123;
+char _cg126;
+char _cg132;
+char _cg154;
+char _cg141;
+char _cg147;
+char _cg151;
+char _cg155;
+char g29_e1;
+char g35_e2;
+char g48_e1;
+char g54_e2;
+char g73_e1;
+char g79_e2;
+char g93_e1;
+char g99_e2;
+char g127_e1;
+char g133_e2;
+char g142_e1;
+char g148_e2;
+char g12_e1;
+char g24_e2;
+char g56_e3;
+char g68_e4;
+char g88_e5;
+char g113_e6;
+char g137_e7;
+char g157_e8;
+int _PRE_GO;
+void reset(){
+   _GO = 1;
+   _PRE_GO = 0;
+   PRE_g3 = 0;
+   PRE_g6 = 0;
+   PRE_g10 = 0;
+   PRE_g15 = 0;
+   PRE_g18 = 0;
+   PRE_g22 = 0;
+   PRE_g30 = 0;
+   PRE_g33 = 0;
+   PRE_g37 = 0;
+   PRE_g40 = 0;
+   PRE_g49 = 0;
+   PRE_g52 = 0;
+   PRE_g59 = 0;
+   PRE_g61 = 0;
+   PRE_g64 = 0;
+   PRE_g74 = 0;
+   PRE_g77 = 0;
+   PRE_g81 = 0;
+   PRE_g84 = 0;
+   PRE_g94 = 0;
+   PRE_g97 = 0;
+   PRE_g101 = 0;
+   PRE_g104 = 0;
+   PRE_g108 = 0;
+   PRE_g115 = 0;
+   PRE_g117 = 0;
+   PRE_g120 = 0;
+   PRE_g128 = 0;
+   PRE_g131 = 0;
+   PRE_g143 = 0;
+   PRE_g146 = 0;
+   PRE_g150 = 0;
+   PRE_g153 = 0;
+   return;
+}
+void tick(){
+   if(_PRE_GO == 1){
+      _GO = 0;
+   }
+   {
+      g0 = _GO;
+      g1 = g0;
+      g11 =(PRE_g10);
+      _cg11 = 1;
+      g7 =(PRE_g6);
+      _cg7 = 1;
+      g2 =(g1||(g11&&_cg11)||(g7&&_cg7));
+      if(g2){
+         ap = 0;
+         as_accept = 0;
+         ap_accept = 0;
+      }
+      g25 = g0;
+      g13 = g0;
+      g19 =(PRE_g18);
+      _cg19 = 1;
+      g23 =(PRE_g22);
+      _cg23 = 1;
+      g14 =(g13||(g19&&_cg19)||(g23&&_cg23));
+      if(g14){
+         vp = 0;
+         vp_accept = 0;
+         vs_accept = 0;
+      }
+      g75 =(PRE_g74);
+      if(g75){
+         VRP = 0;
+      }
+      g82 =(PRE_g81);
+      _cg82 = 1;
+      g85 =(PRE_g84);
+      _cg85 =(t_vrp >= (to_vrp - 2));
+      g86 =(g85&&(!(_cg85)));
+      _cg86 =(t_vrp < (to_vrp - 2));
+      g87 =(g86&&_cg86);
+      if(g87){
+         t_vrp++;
+      }
+      g83 =((g82&&_cg82)||g87);
+      if(g83){
+         VRP = 1;
+      }
+      g144 =(PRE_g143);
+      if(g144){
+         URI = 0;
+      }
+      g151 =(PRE_g150);
+      _cg151 = 1;
+      g154 =(PRE_g153);
+      _cg154 =(t_uri >= (to_uri - 2));
+      g155 =(g154&&(!(_cg154)));
+      _cg155 =(t_uri < (to_uri - 2));
+      g156 =(g155&&_cg155);
+      if(g156){
+         t_uri++;
+      }
+      g152 =((g151&&_cg151)||g156);
+      if(g152){
+         URI = 1;
+      }
+      g16 =(PRE_g15);
+      g16b = g16;
+      _cg16 =((!(VRP))&(!(URI))&vs);
+      g17 =(g16b&&_cg16);
+      if(g17){
+         vs_accept = 1;
+         vp = 1;
+      }
+      g41 =(PRE_g40);
+      g41b = g41;
+      _cg41 = vs_accept;
+      g53 =(PRE_g52);
+      _cg53 = 1;
+      g54 =(g53&&_cg53);
+      if(g54){
+         _trig2 = 1;
+      }
+      g42 =(g41b&&(!(_cg41)));
+      _cg42 =(t_avi < (to_avi - 2));
+      g44 =(g42&&(!(_cg42)));
+      _cg44 =(t_avi >= (to_avi - 2));
+      g45 =(g44&&_cg44);
+      if(g45){
+         AVI_p = 1;
+         _trig2 = 0;
+      }
+      g46 = g45;
+      g50 =(PRE_g49);
+      if(g50){
+         AVI = 0;
+      }
+      g47 =(g46||g50);
+      _cg47 = _trig2;
+      g49 =(g47&&(!(_cg47)));
+      g48_e1 =(!(g49));
+      g48 =(g47&&_cg47);
+      g51 = g45;
+      g52 =(g51||(g53&&(!(_cg53))));
+      g54_e2 =(!(g52));
+      g55 =((g48_e1||g48)&&(g54_e2||g54)&&(g48||g54));
+      g26 =(g25||(g41b&&_cg41)||g55);
+      if(g26){
+         AVI = 0;
+         t_avi = 0;
+         _trig = 0;
+      }
+      g38 =(PRE_g37);
+      _cg38 = 1;
+      g43 =(g42&&_cg42);
+      if(g43){
+         t_avi++;
+      }
+      g39 =((g38&&_cg38)||g43);
+      if(g39){
+         AVI = 1;
+      }
+      g95 =(PRE_g94);
+      if(g95){
+         AEI_p = 0;
+      }
+      g31 =(PRE_g30);
+      if(g31){
+         AVI_p = 0;
+      }
+      g65 =(PRE_g64);
+      _cg65 =(t_pvarp >= (to_pvarp - 1));
+      g57 = g0;
+      g58 =((g65&&_cg65)||g57);
+      if(g58){
+         PVARP = 0;
+         t_pvarp = 0;
+      }
+      g62 =(PRE_g61);
+      _cg62 = 1;
+      g66 =(g65&&(!(_cg65)));
+      _cg66 =(t_pvarp < (to_pvarp - 1));
+      g67 =(g66&&_cg66);
+      if(g67){
+         t_pvarp++;
+      }
+      g63 =((g62&&_cg62)||g67);
+      if(g63){
+         PVARP = 1;
+      }
+      g4 =(PRE_g3);
+      g4b = g4;
+      _cg4 =((!(PVARP))&(!(AVI))&as&(!(AVI_p)));
+      g5 =(g4b&&_cg4);
+      if(g5){
+         as_accept = 1;
+         ap = 1;
+      }
+      g105 =(PRE_g104);
+      g105b = g105;
+      _cg105 = as_accept;
+      g106 =(g105b&&(!(_cg105)));
+      _cg106 =(t_aei >= (to_aei - 1));
+      g107 =(g106&&_cg106);
+      if(g107){
+         AEI = 0;
+         AEI_p = 1;
+      }
+      g8 =(g4b&&(!(_cg4)));
+      _cg8 =(AEI_p&(!(AVI))&(!(vs)));
+      g3 =((g8&&(!(_cg8)))||g2);
+      g6 =(g5||(g7&&(!(_cg7))));
+      g9 =(g8&&_cg8);
+      if(g9){
+         ap_accept = 1;
+         ap = 1;
+      }
+      g10 =(g9||(g11&&(!(_cg11))));
+      g20 =(g16b&&(!(_cg16)));
+      _cg20 =(AVI_p&(!(URI))&(!(as)));
+      g15 =((g20&&(!(_cg20)))||g14);
+      g18 =(g17||(g19&&(!(_cg19))));
+      g21 =(g20&&_cg20);
+      if(g21){
+         vp_accept = 1;
+         vp = 1;
+      }
+      g22 =(g21||(g23&&(!(_cg23))));
+      g27 = g26;
+      g34 =(PRE_g33);
+      g34b = g34;
+      _cg34 =(ap_accept|as_accept);
+      g35 =(g34b&&_cg34);
+      if(g35){
+         _trig = 1;
+      }
+      g28 =(g27||g31);
+      _cg28 = _trig;
+      g29 =(g28&&_cg28);
+      g30 =(g28&&(!(_cg28)));
+      g32 = g26;
+      g33 =(g32||(g34b&&(!(_cg34))));
+      g29_e1 =(!(g30));
+      g35_e2 =(!(g33));
+      g36 =((g29_e1||g29)&&(g35_e2||g35)&&(g29||g35));
+      g37 =(g36||(g38&&(!(_cg38))));
+      g40 =(g39||(g44&&(!(_cg44))));
+      g60 =(PRE_g59);
+      g60b = g60;
+      _cg60 =(vp_accept|vs_accept);
+      g59 =(g58||(g60b&&(!(_cg60))));
+      g61 =((g60b&&_cg60)||(g62&&(!(_cg62))));
+      g64 =(g63||(g66&&(!(_cg66))));
+      g69 = g0;
+      g70 =(g69||(g85&&_cg85));
+      if(g70){
+         t_vrp = 0;
+         _trig3 = 0;
+      }
+      g71 = g70;
+      g78 =(PRE_g77);
+      g78b = g78;
+      _cg78 =(vp_accept|vs_accept);
+      g79 =(g78b&&_cg78);
+      if(g79){
+         _trig3 = 1;
+      }
+      g72 =(g71||g75);
+      _cg72 = _trig3;
+      g73 =(g72&&_cg72);
+      g74 =(g72&&(!(_cg72)));
+      g76 = g70;
+      g77 =((g78b&&(!(_cg78)))||g76);
+      g73_e1 =(!(g74));
+      g79_e2 =(!(g77));
+      g80 =((g73_e1||g73)&&(g79_e2||g79)&&(g73||g79));
+      g81 =(g80||(g82&&(!(_cg82))));
+      g84 =(g83||(g86&&(!(_cg86))));
+      g89 = g0;
+      g109 =(PRE_g108);
+      g109b = g109;
+      _cg109 = as_accept;
+      g110 =(g109b&&(!(_cg109)));
+      _cg110 =(!(as_accept));
+      g90 =((g105b&&_cg105)||g89||(g110&&_cg110));
+      if(g90){
+         AEI = 0;
+         t_aei = 0;
+         _trig4 = 0;
+      }
+      g91 = g90;
+      g98 =(PRE_g97);
+      g98b = g98;
+      _cg98 =(vp_accept|vs_accept);
+      g99 =(g98b&&_cg98);
+      if(g99){
+         _trig4 = 1;
+      }
+      g92 =(g91||g95);
+      _cg92 = _trig4;
+      g93 =(g92&&_cg92);
+      g94 =(g92&&(!(_cg92)));
+      g96 = g90;
+      g97 =(g96||(g98b&&(!(_cg98))));
+      g93_e1 =(!(g94));
+      g99_e2 =(!(g97));
+      g100 =((g93_e1||g93)&&(g99_e2||g99)&&(g93||g99));
+      g102 =(PRE_g101);
+      _cg102 = 1;
+      g101 =(g100||(g109b&&_cg109)||(g102&&(!(_cg102))));
+      g111 =(g106&&(!(_cg106)));
+      _cg111 =(t_aei < (to_aei - 1));
+      g112 =(g111&&_cg111);
+      if(g112){
+         t_aei++;
+      }
+      g103 =((g102&&_cg102)||g112);
+      if(g103){
+         AEI = 1;
+      }
+      g104 =(g103||(g111&&(!(_cg111))));
+      g108 =((g110&&(!(_cg110)))||g107);
+      g114 = g0;
+      if(g114){
+         LRI = 0;
+         LRI_p = 0;
+         t_lri = 0;
+      }
+      g116 =(PRE_g115);
+      g116b = g116;
+      _cg116 =(vp_accept|vs_accept);
+      g115 =((g116b&&(!(_cg116)))||g114);
+      g118 =(PRE_g117);
+      _cg118 = 1;
+      g117 =((g116b&&_cg116)||(g118&&(!(_cg118))));
+      g121 =(PRE_g120);
+      g121b = g121;
+      _cg121 = vp;
+      g122 =(g121b&&_cg121);
+      if(g122){
+         t_lri = 0;
+      }
+      g123 =(g121b&&(!(_cg121)));
+      _cg123 =(t_lri >= (to_lri - 2));
+      g135 =(g123&&(!(_cg123)));
+      _cg135 =(t_lri < (to_lri - 2));
+      g136 =(g135&&_cg135);
+      if(g136){
+         t_lri++;
+      }
+      g132 =(PRE_g131);
+      _cg132 = 1;
+      g133 =(g132&&_cg132);
+      if(g133){
+         _trig5 = 1;
+      }
+      g129 =(PRE_g128);
+      if(g129){
+         LRI = 0;
+      }
+      g124 =(g123&&_cg123);
+      if(g124){
+         LRI_p = 1;
+         t_lri = 0;
+         _trig5 = 0;
+      }
+      g125 = g124;
+      g126 =(g129||g125);
+      _cg126 = _trig5;
+      g128 =(g126&&(!(_cg126)));
+      g127_e1 =(!(g128));
+      g127 =(g126&&_cg126);
+      g130 = g124;
+      g131 =(g130||(g132&&(!(_cg132))));
+      g133_e2 =(!(g131));
+      g134 =((g127_e1||g127)&&(g133_e2||g133)&&(g127||g133));
+      g119 =(g122||g136||g134||(g118&&_cg118));
+      if(g119){
+         LRI_p = 0;
+         LRI = 1;
+      }
+      g120 =(g119||(g135&&(!(_cg135))));
+      g138 = g0;
+      g139 =(g138||(g154&&_cg154));
+      if(g139){
+         t_uri = 0;
+         _trig6 = 0;
+      }
+      g140 = g139;
+      g147 =(PRE_g146);
+      g147b = g147;
+      _cg147 =(vp_accept|vs_accept);
+      g148 =(g147b&&_cg147);
+      if(g148){
+         _trig6 = 1;
+      }
+      g141 =(g140||g144);
+      _cg141 = _trig6;
+      g142 =(g141&&_cg141);
+      g143 =(g141&&(!(_cg141)));
+      g145 = g139;
+      g146 =(g145||(g147b&&(!(_cg147))));
+      g142_e1 =(!(g143));
+      g148_e2 =(!(g146));
+      g149 =((g142_e1||g142)&&(g148_e2||g148)&&(g142||g148));
+      g150 =((g151&&(!(_cg151)))||g149);
+      g153 =(g152||(g155&&(!(_cg155))));
+      g12_e1 =(!((g3||g6||g10)));
+      g24_e2 =(!((g15||g18||g22)));
+      g56_e3 =(!((g33||g30||g37||g40||g52||g49)));
+      g68_e4 =(!((g59||g61||g64)));
+      g88_e5 =(!((g77||g74||g81||g84)));
+      g113_e6 =(!((g97||g94||g101||g104||g108)));
+      g137_e7 =(!((g115||g117||g120||g131||g128)));
+      g157_e8 =(!((g146||g143||g150||g153)));
+   }
+   PRE_g3 = g3;
+   PRE_g6 = g6;
+   PRE_g10 = g10;
+   PRE_g15 = g15;
+   PRE_g18 = g18;
+   PRE_g22 = g22;
+   PRE_g30 = g30;
+   PRE_g33 = g33;
+   PRE_g37 = g37;
+   PRE_g40 = g40;
+   PRE_g49 = g49;
+   PRE_g52 = g52;
+   PRE_g59 = g59;
+   PRE_g61 = g61;
+   PRE_g64 = g64;
+   PRE_g74 = g74;
+   PRE_g77 = g77;
+   PRE_g81 = g81;
+   PRE_g84 = g84;
+   PRE_g94 = g94;
+   PRE_g97 = g97;
+   PRE_g101 = g101;
+   PRE_g104 = g104;
+   PRE_g108 = g108;
+   PRE_g115 = g115;
+   PRE_g117 = g117;
+   PRE_g120 = g120;
+   PRE_g128 = g128;
+   PRE_g131 = g131;
+   PRE_g143 = g143;
+   PRE_g146 = g146;
+   PRE_g150 = g150;
+   PRE_g153 = g153;
+   _PRE_GO = _GO;
+   return;
 }
